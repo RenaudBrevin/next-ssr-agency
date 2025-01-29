be0a129ed1007125a79b6b2633c75d2a36833584
Thomas Filhol <thomasfilhol@MacBook-Air-de-Thomas-3.local>
2025-01-29 16:30:07 +0100

    Last changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,27 @@ import Image from 'next/image';
 import './page.css';
 import ScrollSlider from '@/components/02-sections/scroll-list';
 import Clients from '@/components/02-sections/clients';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
       <section className='hero flex flex-col items-center'>
-        <div className="flex flex-col items-center my-auto">
-          <h1 className='hero--title text-6xl font-bold'><span className='text-gold'> Paris </span> Web Agency</h1>
+        <div className="flex flex-col items-center">
+          <h1 className='hero--title text-6xl font-bold mb-4'><span className='text-gold'> Paris </span> Web Agency</h1>
           <ScrollSlider />
-          <button className='bg-white flex items-center gap-5 px-5 py-3 text-black font-extrabold rounded-2xl mt-12'>
-            <span>Share a project</span>
-            <Image
-            className=""
-            src="/right-arrow.png"
-            alt="Next.js logo"
-            width={15}
-            height={30}
-            priority
-          />
-          </button>
+          <Link href="/projects" className='bg-white flex items-center gap-5 px-5 py-3 text-black font-extrabold rounded-2xl mt-12'>
+              <span>Our Projects</span>
+              <Image
+              className=""
+              src="/right-arrow.png"
+              alt="Next.js logo"
+              width={15}
+              height={30}
+              priority
+            />
+          </Link>
+          <p className="quote text-white text-3xl font-bold mt-8">"Elite Digital Crafting for Visionary Brands."</p>
         </div>
         <Clients />
       </section>
--- src/components/02-sections/header.tsx
+++ src/components/02-sections/header.tsx
@@ -1,17 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function header() {
     return (
-    <nav className="flex items-center container py-6">
-        <Image
-            className=""
-            src="/logo-riche.gif"
-            alt="Next.js logo"
-            width={120}
-            height={30}
-            priority
-        />
-        <ul className="flex items-center font-bold justify-center border border-white rounded-full bg-lightBlack mx-auto">
+    <nav className="flex items-center justify-between container py-6">
+        <Link href="/">
+            <Image
+                className=""
+                src="/logo-riche.gif"
+                alt="Next.js logo"
+                width={120}
+                height={30}
+                priority
+            />
+        </Link>
+        {/* <ul className="flex items-center font-bold justify-center border border-white rounded-full bg-lightBlack mx-auto">
             <li className="px-6 py-3.5">
             <a href="">Projects</a>
             </li>
@@ -24,8 +27,12 @@ export default function header() {
             <li className="px-6 py-3.5">
             <a href="">Projects</a>
             </li>
-        </ul>
-        <button className="contact ml-0 rounded-2xl border border-gold text-gold px-4 py-3 bg-lightBlack font-extrabold">Contact</button>
+        </ul> */}
+        <Link 
+            href="/contact"
+            className="contact ml-0 rounded-2xl border border-gold text-gold px-4 py-3 bg-lightBlack font-extrabold"> 
+            Contact
+        </Link>
     </nav>
     )
 }
