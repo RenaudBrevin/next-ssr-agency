95d312b255774d2757eb6c509b926f7af8ccd418
RENAUD Brevin <renaudbrevin@gmail.com>
2025-01-29 16:14:04 +0100

    Projects database - first part

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,16 +5,16 @@ const prisma = new PrismaClient()
 async function main() {
     await prisma.project.create({
         data: {
-            title: "Application de gestion de tâches",
-            description: "Une application full-stack pour gérer les tâches quotidiennes.",
-            projectUrl: "https://www.example.com/project/task-manager",
+            title: "Beauetrra - Refonte site e-commerce",
+            description: "Beauterra est un site e-commerce pour de la vente B2B vers des pharmacies. Une partie du site est une vitrine B2C pour présenter ses produits au grand public et une seconde partie gère la prise de commande pour les pharmacies cliente.",
+            projectUrl: "https://beauterra.com/",
             projectDetails: {
                 create: {
-                    context: "Un projet pour une gestion efficace des tâches personnelles.",
-                    objectives: "Permettre aux utilisateurs de créer, modifier et supprimer des tâches.",
-                    technologies: ["React", "Node.js", "PostgreSQL", "Prisma"],
-                    results: "Les utilisateurs peuvent gérer leurs tâches avec une interface simple et claire.",
-                    imagesUrl: ["https://www.example.com/images/task-manager.png"],
+                    context: "Beauterra souhaitait refondre son site afin d'améliorer l'accessibilité et l'expérience utilisateur en général sur son site.",
+                    objectives: "L'objectif de cette refonte est de données aux equipe marketing la possibilité de personnaliser chaque page de leur site et de pouvoir gérer les commandes de façon simplifé avec un design élégant et en accord avec leur charte graphique.",
+                    technologies: ["Wordpress", "WooCommerce", "Elementor", "Figma"],
+                    results: "Le nombre de commandes a augmenté de 30% depuis la refonte du site. Les visites de particuliers pour se renseigner sur les produits ont augmenté de 20%.",
+                    imagesUrl: ["beauterra.webp"],
                 }
             }
         }
@@ -22,33 +22,16 @@ async function main() {
 
     await prisma.project.create({
         data: {
-            title: "Site web de commerce électronique",
-            description: "Un site pour acheter des produits en ligne.",
-            projectUrl: "https://www.example.com/project/ecommerce-site",
+            title: "Gamme Meteogaz - Landing page",
+            description: "Landing page créée pour mettre en avant les produits de la gamme Meteogaz en prévision d'une publicité télévisée.",
+            projectUrl: "https://gamme-meteogaz.fr/",
             projectDetails: {
                 create: {
-                    context: "Création d'un site de e-commerce pour une boutique locale.",
-                    objectives: "Permettre l'achat de produits en ligne, gérer les paiements et les stocks.",
-                    technologies: ["Next.js", "Stripe", "MongoDB", "Prisma"],
-                    results: "Le site permet aux utilisateurs de parcourir et acheter des produits avec un système de paiement intégré.",
-                    imagesUrl: ["https://www.example.com/images/ecommerce-site.png"]
-                }
-            }
-        }
-    })
-
-    await prisma.project.create({
-        data: {
-            title: "Portfolio personnel",
-            description: "Un portfolio pour présenter des projets web et des compétences en développement.",
-            projectUrl: "https://www.example.com/project/personal-portfolio",
-            projectDetails: {
-                create: {
-                    context: "Créer un site web pour présenter mes projets et compétences.",
-                    objectives: "Permettre aux visiteurs de découvrir mes projets et de me contacter facilement.",
-                    technologies: ["HTML", "CSS", "JavaScript", "React"],
-                    results: "Un portfolio interactif avec une section de contact et un aperçu des projets réalisés.",
-                    imagesUrl: ["https://www.example.com/images/portfolio.png"]
+                    context: "Meteogaz souhaitait une page dédiée pour présenter sa gamme de produits et capter l'attention des visiteurs après la diffusion de sa publicité TV.",
+                    objectives: "L'objectif est d'offrir une vitrine claire et percutante, optimisée pour une navigation rapide et une compatibilité mobile parfaite.",
+                    technologies: ["Wordpress", "Elementor", "Figma", "Matomo"],
+                    results: "Le site a bénéficié d'une forte hausse de trafic après la campagne TV, plus de 20 000 visites sur le site.",
+                    imagesUrl: ["meteogaz.webp"]
                 }
             }
         }
