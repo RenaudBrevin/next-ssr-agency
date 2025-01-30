2f3d5d3b47eed3c448786a0f17dc25bfafea2612
RENAUD Brevin <renaudbrevin@gmail.com>
2025-01-30 14:33:33 +0100

    Setup filter and change DB

---
+++ src/components/01-atoms/Filter.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Filter = ({ label, options, selected, selection }: { label: string, options: string[], selected: (value: string | null) => void, selection: string | null }) => {
+    return (
+        <div>
+            <label htmlFor="technologyFilter" className="mr-4 font-bold">{label}:</label>
+            <select
+                id="technologyFilter"
+                value={selection || ''}
+                onChange={(e) => selected(e.target.value || null)}
+                className="bg-[#2e2e2e] border-0 rounded-md p-2"
+            >
+                <option value="">Toutes</option>
+                {options.map((tech, index) => (
+                    <option key={index} value={tech}>
+                        {tech}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
+
+export default Filter;
--- src/components/02-sections/ProjectsComponents.tsx
+++ src/components/02-sections/ProjectsComponents.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
 import { Project } from '@/app/types';
 import ProjectCard from '@/components/01-atoms/ProjectCard';
+import Filter from '@/components/01-atoms/Filter';
 
 const ProjectsComponents = ({ projects }: { projects: Project[] }) => {
+    const allTechnologies = Array.from(
+        new Set(projects.flatMap(project => project.projectDetails?.technologies || []))
+    );
+
+    const allCategories = Array.from(
+        new Set(projects.flatMap(project => project.projectDetails?.categories || []))
+    );
+
+    const [selectedTech, setSelectedTech] = useState<string | null>(null);
+    const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+    const filteredProjects = selectedTech
+        ? projects.filter(project => project.projectDetails?.technologies.includes(selectedTech))
+        : projects;
+
     return (
-        <div className="flex flex-wrap gap-12">
-            {projects.map((project) => (
-                <ProjectCard
-                    key={project.id}
-                    project={project}
-                />
-            ))}
+        <div>
+            <div className="mb-8 flex gap-8 items-center">
+                <Filter label="Technologies" options={allTechnologies} selected={setSelectedTech} selection={selectedTech} />
+                <Filter label="Categories" options={allCategories} selected={setSelectedCategory} selection={selectedCategory} />
+            </div>
+
+            <div className="flex flex-wrap gap-12 justify-center">
+                {filteredProjects.map((project) => (
+                    <ProjectCard
+                        key={project.id}
+                        project={project}
+                    />
+                ))}
+            </div>
         </div>
-    )
+    );
 };
 
 export default ProjectsComponents;
