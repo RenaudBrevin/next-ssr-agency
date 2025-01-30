d50c701370b97ac5532014d7a47ad9dd227d0412
Thomas Filhol <thomasfilhol@MacBook-Air-de-Thomas-3.local>
2025-01-30 09:53:51 +0100

    Adding some projects to the website

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -22,6 +22,23 @@ async function main() {
 
     await prisma.project.create({
         data: {
+            title: "MDBA - Refonte site principal",
+            description: "Leader européen de production de missiles, MDBA souhaitait refondre son site principal pour le rendre plus accessible et optimisé pour le référencement, tout en mettant en avant ses produits et innovations.",
+            projectUrl: "https://www.mbda-systems.com/",
+            projectDetails: {
+                create: {
+                    context: "Le site devait être modernisé pour améliorer l'accessibilité et le référencement, tout en valorisant les produits et innovations de MBDA.",
+                    objectives: "Offrir une plateforme moderne et dynamique répondant aux standards actuels d'accessibilité et de SEO.",
+                    technologies: ["Twig", "Drupal", "Tailwind", "Figma"],
+                    results: "Depuis la refonte, le trafic a augmenté de 40%, et les visites de particuliers intéressés par les produits ont progressé de 20%.",
+                    imagesUrl: ["MBDA.png"],
+                }
+            }
+        }        
+    })
+
+    await prisma.project.create({
+        data: {
             title: "Gamme Meteogaz - Landing page",
             description: "Landing page créée pour mettre en avant les produits de la gamme Meteogaz en prévision d'une publicité télévisée.",
             projectUrl: "https://gamme-meteogaz.fr/",
@@ -32,6 +49,40 @@ async function main() {
                     technologies: ["Wordpress", "Elementor", "Figma", "Matomo"],
                     results: "Le site a bénéficié d'une forte hausse de trafic après la campagne TV, plus de 20 000 visites sur le site.",
                     imagesUrl: ["meteogaz.webp"]
+                }
+            }
+        }
+    })
+
+    await prisma.project.create({
+        data: {
+            title: "Coque de Nacre - site e-commerce",
+            description: "Coque de Nacre est une boutique en ligne spécialisée dans la vente de bijoux en nacre. Le site propose une large gamme de bijoux pour femmes et hommes.",
+            projectUrl: "https://www.bijouxcn.com/Page.aspx",
+            projectDetails: {
+                create: {
+                    context: "Coque de Nacre souhaitait moderniser son site e-commerce pour améliorer l'expérience utilisateur et augmenter les ventes.",
+                    objectives: "L'objectif est de moderniser le site pour le rendre plus attractif et plus facile à utiliser pour les visiteurs.",
+                    technologies: ["Twig", "Tailwind", "Client Relou"],
+                    results: "Le site a bénéficié d'une hausse de 30% des ventes après la refonte.",
+                    imagesUrl: ["CDN.png"]
+                }
+            }
+        }
+    })
+
+    await prisma.project.create({
+        data: {
+            title: "Fidesio - Refonte site de l'agence",
+            description: "Fidesio est une agence web. Le site de l'agence a été refondu pour mettre en avant les compétences de l'équipe et les projets réalisés.",
+            projectUrl: "https://www.bijouxcn.com/Page.aspx",
+            projectDetails: {
+                create: {
+                    context: "Fidesio souhaitait moderniser son site pour accroître sa visibilité.",
+                    objectives: "Un design avant-gardiste et une navigation intuitive pour mettre en avant les compétences de l'équipe et les projets réalisés.",
+                    technologies: ["Twig", "Tailwind"],
+                    results: "Le site a bénéficié d'une hausse de 30% de clients après la refonte.",
+                    imagesUrl: ["fidesio.png"]
                 }
             }
         }
